9de4f4e6b62c9d3b0ea3e2832ae6e7e0b8497c4a
ahmedeltayyeb <eltayyebahmed@gmail.com>
2024-12-23 23:11:19 +0100

    Can get data from a job url

--- scraping/main.py
+++ scraping/main.py
@@ -1,16 +1,23 @@
-# import yaml
-from scraping import job_url_to_title, generate_url
+from scraping import generate_job_url_list, generate_url, job_url_to_title, job_url_to_content
+from config import selenium_config
 
 PARAMETER_FILE = "params.yaml"
 
 
 def main():
-    # with open(PARAMETER_FILE, "r") as file:
-    #     params = yaml.safe_load(file)
+    driver = selenium_config()
+    try:
+        url = generate_url()
+        job_links = generate_job_url_list(url, driver)
+        job_url_to_title(job_links, "data/url_title.json")
 
-    url = generate_url()
-    job_url_to_title(url, "data/url_title")
-
+        # URL will be based on the url of the job, which the user wants to generate a cv for.
+        # This should only be for the LLM whihc generates the CV.
+        # Url used is just for testing
+        job_url_to_content("https://www.stepstone.de/jobs--Cloud-Software-Engineer-IoT-Data-Streaming-for-AI-m-w-d-m-f-d-Heilbronn-Schwarz-Corporate-Solutions--11657118-inline.html"
+                           , driver, "data/job_content.json")
+    finally:
+        driver.quit()
 
 if __name__ == "__main__":
     main()
--- scraping/scraping.py
+++ scraping/scraping.py
@@ -1,11 +1,13 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from config import selenium_config
 from utils import save_to_json
 
 
 def generate_url():
+    '''
+    generates a url based on the user input for the job title and location
+    '''
     # Get user input for job title and location
     user_job_title = input("Enter the job title to search for: ").replace(" ", "-").lower()
     user_location = input("Enter the location to search in: ").replace(" ", "-").lower()
@@ -13,26 +15,32 @@ def generate_url():
     # Construct the URL
     base_url = "https://www.stepstone.de/work"
     search_url = f"{base_url}/{user_job_title}/in-{user_location}?radius=30&searchOrigin=Resultlist_top-search&rsearch=1"
-
     print(f"Searching URL: {search_url}")
     return search_url
 
 
-def job_url_to_title(desired_url, json_path):
-    driver = selenium_config()
-    try:
-        # Open the desired URL
-        driver.get(desired_url)
-        # Wait for the elements to load
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "res-1foik6i"))
-        )
-        # Get the url of the jobs in this page
-        link_element = driver.find_elements(By.CLASS_NAME, "res-1foik6i")
+def generate_job_url_list(desired_url, driver):
+    '''
+    takes the desired_url from generate_url() and returns the top 25 search results
+    '''
+    # Open the desired URL
+    driver.get(desired_url)
+    # Wait for the elements to load
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "res-1foik6i"))
+    )
+    # Get the url of the jobs in this page
+    link_elements = driver.find_elements(By.CLASS_NAME, "res-1foik6i")
+    return link_elements
 
+
+def job_url_to_title(job_links, json_path):
+        '''
+        Takes the link_elements from generate_job_url_list() and returns the job titles with the urls of the jobs in a json file
+        '''
         #Dict of url: title
         url_title = {}
-        for link in link_element:
+        for link in job_links:
             job_link = link.get_attribute("href")
             job_title = link.find_element(By.CLASS_NAME, "res-nehv70")
             url_title[job_link] = job_title.text
@@ -40,6 +48,33 @@ def job_url_to_title(desired_url, json_path):
         save_to_json(url_title, json_path)
         print(f"Scraped {len(url_title)} jobs. Results saved to 'data/url_title.json'.")
 
-    finally:
-        # Quit the browser
-        driver.quit()
+
+def job_url_to_content(job_link, driver, json_path):
+    '''
+    Takes the link of the job which the user wants to generate the CV and/or cover letter for and produces an output whihc could be used by the LLM
+    '''
+    driver.get(job_link)
+    # Wait for the elements to load
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "job-ad-display-11k0r7z"))
+    )
+
+    # Scrape job details
+    company_name = driver.find_element(By.CSS_SELECTOR, "#JobAdContent > div > div > div > div > div > div > div > div.js-listing-header.job-ad-display-kyg8or > article > div > div.job-ad-display-n10qeq > div > div.job-ad-display-1t26un2 > div > ul > li.at-listing__list-icons_company-name.job-ad-display-1f4i9zs > a > span.job-ad-display-11k0r7z > span").text
+    full_part = driver.find_element(By.CSS_SELECTOR, "#JobAdContent > div > div > div > div > div > div > div > div.js-listing-header.job-ad-display-kyg8or > article > div > div.job-ad-display-n10qeq > div > div.job-ad-display-1t26un2 > div > ul > li.at-listing__list-icons_work-type.job-ad-display-1f4i9zs > span > span.job-ad-display-11k0r7z > span").text
+    published = driver.find_element(By.CSS_SELECTOR,"#JobAdContent > div > div > div > div > div > div > div > div.js-listing-header.job-ad-display-kyg8or > article > div > div.job-ad-display-n10qeq > div > div.job-ad-display-1t26un2 > div > ul > li.at-listing__list-icons_date.job-ad-display-1f4i9zs > span > span.job-ad-display-11k0r7z > span").text
+    published = published[11:]
+
+    # Scrape the job description
+    spans = driver.find_elements(By.CLASS_NAME, "job-ad-display-1cat3iu")
+    all_texts = [span.text for span in spans]
+    content = "\n".join(all_texts)
+
+    content_dict = {
+        "Company name" : company_name,
+        "Full or Part time?" : full_part,
+        "Date published" : published,
+        "Content of job description": content
+    }
+
+    save_to_json(content_dict, json_path)
